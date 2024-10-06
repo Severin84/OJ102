a3e0856125875e41723695860039eaa4f62dbde6
Severin84 <84severin@gmail.com>
2024-10-06 13:28:55 +0530

    checking login logs

--- server/Controllers/User.js
+++ server/Controllers/User.js
@@ -74,8 +74,9 @@ const login=async(req,res,next)=>{
       if(!isPasswordVaild){
         return res.status(401).json({message:"Invalid Email or Password"});
       }
-      console.log("7")
       console.log(user)
+      console.log("7")
+     
       const {accessToken,refreshToken}=await generateAccessAndRefereshToken(user._id);
       console.log("8")
       const loggedInUser=await User.findById(user._id).select("-password -refreshToken");
